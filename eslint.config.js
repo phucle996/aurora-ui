d9aad59c881725154c5ac53ffd6d4b33c7d81d77
Phuc Le <10k.polime.vnd@gmail.com>
2026-03-09 00:09:41 +0700

    update

--- eslint.config.js
+++ eslint.config.js
@@ -19,5 +19,17 @@ export default defineConfig([
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    rules: {
+      'react-refresh/only-export-components': 'off',
+      'react-hooks/set-state-in-effect': 'off',
+      'react-hooks/purity': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+        },
+      ],
+    },
   },
 ])
